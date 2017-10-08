051ab756bf66a029754394abf6b63469d77a860e
Judy <yanghuanghuang@gmail.com>
2017-10-08 16:43:33 +0800

    trying to test actions

---
+++ tests/test_actions.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import mock
+
+from utils import create_users
+from friends.utils import get_strategy
+from friends.actions import do_invite_friend
+from friends.frameworks.flask.utils import get_helper
+from flask import Blueprint, g, request, jsonify
+
+
+def test_do_invite_friend(app, flask_mongoengine_storage):
+    users = create_users(app, flask_mongoengine_storage)
+    user_a = users[0]
+    user_b = users[1]
+
+    mock_strategy = mock.Mock()
+    mock_strategy.storage = flask_mongoengine_storage
+    do_invite_friend(mock_strategy, from_user=user_a.email,
+                     to_user_email=user_b.email,
+                     message="Hello! I'm Harry!")
---
+++ tests/utils.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+def create_users(app, flask_mongoengine_storage):
+    User = flask_mongoengine_storage.user
+    with app.app_context():
+        users = [
+            ('judy@ff.com', 'judy', None, True),
+            ('harry@ff.com', 'harry', None, True),
+            ('sha@ff.com', 'sha', None, True),
+            ('walle@ff.com', 'walle', None, True),
+            ('tony@ff.com', 'tony', None, False),]
+
+        for u in users:
+            user = User(email=u[0], username=u[1], password=u[2],
+                        active=u[3])
+            user.save()
+    return User.objects
