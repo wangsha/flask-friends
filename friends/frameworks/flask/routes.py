13cf4d9e1b681feff6b43c961f823bf4c98d80d3
Sha <sha@detalytics.com>
2017-11-24 18:57:49 +0800

    enhance response ui

--- friends/frameworks/flask/routes.py
+++ friends/frameworks/flask/routes.py
@@ -11,6 +11,86 @@ from friends.frameworks.flask.utils import load_strategy
 
 friends_blueprint = Blueprint('friends', __name__)
 
+def okay_response():
+    resp = """
+    <!DOCTYPE html>
+<html lang="en">
+<title>Success</title>
+<meta name="viewport" content="width=device-width">
+
+<head>
+    <meta charset="utf-8">
+    <title>Ok</title>
+    <style>
+        ::-moz-selection {
+        background: #b3d4fc;
+        text-shadow: none;
+        }
+
+        ::selection {
+        background: #b3d4fc;
+        text-shadow: none;
+        }
+
+        html {
+        padding: 30px 10px;
+        font-size: 20px;
+        line-height: 1.4;
+        color: #737373;
+        background: #f0f0f0;
+        -webkit-text-size-adjust: 100%;
+        -ms-text-size-adjust: 100%;
+        }
+
+        html,
+        input {
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+        }
+
+        body {
+        max-width: 500px;
+        _width: 500px;
+        padding: 30px 20px 50px;
+        border: 1px solid #b3b3b3;
+        border-radius: 4px;
+        margin: 0 auto;
+        box-shadow: 0 1px 10px #a7a7a7, inset 0 1px 0 #fff;
+        background: #fcfcfc;
+        }
+
+        h1 {
+        margin: 0 10px;
+        font-size: 50px;
+        text-align: center;
+        }
+
+        h1 span {
+        color: #bbb;
+        }
+
+        h3 {
+        margin: 1.5em 0 0.5em;
+        }
+
+        .container {
+        max-width: 380px;
+        _width: 380px;
+        margin: 0 auto;
+        }
+
+
+
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1>Ok <span>:)</span></h1>
+    <h3>Your request has been recorded.</h3>
+</div>
+</body>
+</html>
+    """
+    return resp
 
 @friends_blueprint.route('/request_friend', methods=('POST',))
 @load_strategy
@@ -26,28 +106,28 @@ def create_friendship():
                      from_user=user,
                      to_user_email=email,
                      message=message)
-    return 'Ok', 200
+    return okay_response(), 200
 
 
 @friends_blueprint.route('/accept/<string:token>', methods=('POST',))
 @load_strategy
 def accept_friend_request(token):
     accept_friendship_request(g.strategy, token)
-    return 'Ok', 200
+    return okay_response(), 200
 
 
 @friends_blueprint.route('/reject/<string:token>', methods=('POST',))
 @load_strategy
 def reject_friend_request(token):
     reject_friendship_request(g.strategy, token)
-    return 'Ok', 200
+    return okay_response(), 200
 
 
 @friends_blueprint.route('/cancel/<string:token>', methods=('POST',))
 @load_strategy
 def cancel_friend_request(token):
     cancel_friendship_request(g.strategy, token)
-    return 'Ok', 200
+    return okay_response(), 200
 
 
 @friends_blueprint.route('/friend_requests', methods=('GET',))
